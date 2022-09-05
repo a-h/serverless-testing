79d98de1a2706c03e7c389613a7d465bc2561b5e
Adrian Hesketh <adrianhesketh@hushmail.com>
2022-09-05 10:07:11 +0100

    feat: added results of cdk init

---
+++ node-count-example-aws-lambda/test/node-count-example-aws-lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NodeCountExampleAwsLambda from '../lib/node-count-example-aws-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/node-count-example-aws-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NodeCountExampleAwsLambda.NodeCountExampleAwsLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
