46eb51a56ab12b939112a84dc3daccf3a2273f15
Adrian Hesketh <a-h@users.noreply.github.com>
2022-12-20 15:09:52 +0000

    Update index.ts

--- node-count-example/src/http/count/getsimple/lambda/index.ts
+++ node-count-example/src/http/count/getsimple/lambda/index.ts
@@ -1,19 +1,12 @@
 import express from "express";
 import serverless from "serverless-http";
-import { DB } from "../../../../db/dynamo"
 import * as countGet from "../"
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 import { requestLogger } from "../../../requestlogger";
 
 const app = express()
 app.use(express.json())
 app.use(requestLogger)
 
-const ddbClient = new DynamoDBClient({ region: process.env.DYNAMODB_REGION })
-const documentClient = DynamoDBDocumentClient.from(ddbClient)
-const db = new DB(documentClient, process.env.TABLE_NAME)
-
-app.get(countGet.route, countGet.create(db.get))
+app.get(countGet.route, countGet.handler)
 
 module.exports.handler = serverless(app);
